e7dd6fc74e5dab52678af10ef5f71c7c0d8d4815
Sohrab Alam Ansari <ansari.shohrab80@github.com>
2021-01-03 18:44:05 +0530

    Init

---
+++ src/app/components/topbar/topbar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss'],
+})
+export class TopbarComponent implements OnInit {
+  firstName: string = 'Sohrab';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
